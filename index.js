ab78e9880c42992a64ed7010d9e084140ceb9dff
Matt Lea <mlea@schematical.com>
2014-04-08 07:25:48 -0500

    damn you cookie middleware

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ var app = njax({
 //njaxBootstrap(app);
 app.locals.partials._navbar = '_navbar';
 app.locals.partials._meta = '_meta';
+app.locals.partials._account = '_account';
+app.use(function(req, res, next){
+    app.model.Account.find({}, function(err, accounts){
+        res.locals.accounts = [];
+        for(var i in accounts){
+            res.locals.accounts[i] = accounts[i].toObject();
+        }
+        return next();
+    })
+
+})
 var routes = require(__dirname + '/lib/routes');
 routes(app)
 app.start();
