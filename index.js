ff96e063d5b664bf116f3785f125f0a37b7a64d5
Matt Lea <mlea@schematical.com>
2014-04-10 05:30:36 -0500

    boosly

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var async = require('async');
 var njax = require('njax');
 var njaxBootstrap = require('njax-bootstrap');
 var app = njax({
@@ -8,14 +9,37 @@ var app = njax({
 app.locals.partials._navbar = '_navbar';
 app.locals.partials._meta = '_meta';
 app.locals.partials._account = '_account';
+app.locals.partials._meta_footer = '_meta_footer';
 app.use(function(req, res, next){
-    app.model.Account.find({}, function(err, accounts){
-        res.locals.accounts = [];
-        for(var i in accounts){
-            res.locals.accounts[i] = accounts[i].toObject();
+    async.series([
+        function(cb){
+            app.model.Account.find({}, function(err, accounts){
+                res.locals.accounts = [];
+                for(var i in accounts){
+                    res.locals.accounts[i] = accounts[i].toObject();
+                }
+                return cb();
+            })
+        },
+        function(cb){
+            //Shitty hack
+            if(req.user){
+                return next();
+            }
+            app.model.Account.findOne({ email:'mlea@schematical.com' }, function(err, account){
+
+                req.login(account, function(err) {
+                    if (err) { return next(err); }
+                    return cb();
+                });
+
+            })
+
         }
-        return next();
-    })
+    ],
+    next
+    );
+
 
 })
 var routes = require(__dirname + '/lib/routes');
